e5ee732ce416d9c77c357e8783ca4ca20dd224b3
ascencio <2020371104@uteq.edu.mx>
2024-07-16 23:03:21 -0600

    inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterModule } from './counter/components/counter.module';
+import { ListModule } from './heroes/components/list.module';
+import { HeroesModule } from './heroes/components/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CounterModule,
+    ListModule,
+    HeroesModule,
+    DbzModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/heroes/components/heroes.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { HeroComponent } from "./hero/hero.component";
+
+
+
+@NgModule({
+  declarations:[
+    HeroComponent
+  ],
+  exports:[
+    HeroComponent
+  ],
+  imports:[
+    CommonModule
+  ]
+})
+
+export class HeroesModule{
+
+}
